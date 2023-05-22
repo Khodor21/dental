5d61dfa18249e22d53bae1a7d44a5abd018da68b
Khodor21 <101439119+Khodor21@users.noreply.github.com>
2023-05-22 20:26:23 +0300

    Add files via upload

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const TeamCard = ({ Name, Age, Desc, Image }) => {
   return (
     <div className="flex justify-center items-center overflow-x-auto p-10">
-      <div className=" rounded-[50px] overflow-hidden shadow-2xl bg-main">
-        <img src={Image} alt="placeholder" className="w-[200px] mx-20 pt-2" />
+      <div className=" rounded-[10px] overflow-hidden shadow-2xl bg-main">
+        <img src={Image} alt="placeholder" className="w-[200px] mx-auto pt-2" />
         <div className="px-4 py-2">
           <h5 className="text-lg font-bold mb-4 text-center text-white">
             {Name}
