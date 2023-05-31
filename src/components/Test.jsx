d471ccef1758cd5e3458b42135ae9560558077b3
Khodor21 <101439119+Khodor21@users.noreply.github.com>
2023-05-31 12:23:53 +0300

    Update Test.jsx

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import Calendar from 'react-big-calendar';
+
+import {  dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { format, getDay, parse, startOfWeek, isWithinInterval } from "date-fns";
 import { enUS } from "date-fns/locale";
